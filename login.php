d1595821fcdfaebdf0f4215535b951453cd84b59
Nuno Mendes <nunomendes163rgmail.com>
2024-03-26 13:10:49 +0000

    Linear gradient do background

--- login.php
+++ login.php
@@ -5,16 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Projeto TI</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=JetBrains Mono' rel='stylesheet'>
     <link rel="stylesheet" href="login.css">
 </head>
   <body>
     <div class="container vh-100 d-flex justify-content-center align-items-center">
         <div class="row">
             <div class="col">
-                <label for="username">Username</label>
-                <input type="username" class="form-control">
+                <label for="username">Utilizador</label>
+                <input type="username" class="form-control" placeholder="Nome de utilizador">
                 <label for="password">Password</label>
-                <input type="password" class="form-control">
+                <input type="password" class="form-control" placeholder="Password">
+                <button class="btn">Submit</button>
             </div>
         </div>
         </div>
